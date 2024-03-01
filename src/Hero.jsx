d01c82e0ca917f58bb98bcdf2cf9df58a4faaa68
Carlos Borrega <c.borrega@proton.me>
2024-03-01 19:10:50 +0000

    added red div to hero section

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
 const Hero = () => {
-  return (
-    <div>
-      <h1>Hero</h1>
-    </div>
-  );
+    return (
+        <div className='h-screen'>
+            <h1>BEYONCE</h1>
+            <h2>TEXAS HOLD 'EM</h2>
+            <div>
+                <div className='red-div bg-custom-red shadow-md shadow-black'>
+                </div>
+            </div>
+        </div>
+
+    );
 };
 
 export default Hero;
