dc7b59d1e9710442e072912c2b38c82d8b788317
JoeCross <krstevskigj@gmail.com>
2024-03-01 21:25:07 +0000

    api call for search result

--- src/API/APICalls.js
+++ src/API/APICalls.js
@@ -1,3 +1,32 @@
-export function GetSongDetails() {
+export async function GetSearchResult(searchQuery) {
+    return;
+    
+    const url = `https://genius-song-lyrics1.p.rapidapi.com/search/?q=${searchQuery}&per_page=20&page=1`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '200704b6c5mshb43ca1d1bafb8dap12a0fejsnf1364f2f2442',
+            'X-RapidAPI-Host': 'genius-song-lyrics1.p.rapidapi.com'
+        }
+    };
 
+    let results = [];
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        //console.log(result);
+        results = result.hits.map((res)=>{
+            return {
+                cover : res.result.header_image_url,
+                name : res.result.title,
+                artist : res.result.artist_names,
+                year : res.result.release_date_for_display
+            }
+        });
+
+    } catch (error) {
+        console.error(error);
+    }
+
+    return results;
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import SearchResults from './SearchResults'
+import { GetSearchResult } from './API/APICalls.js';
+
 
 function App() {
+  const[results, setResults] = useState([]);
+  useEffect(() => {
+
+    async function loadResults(){
+      const res = await GetSearchResult("Beyonce")
+      setResults(res);
+    }
+
+    loadResults(); 
+  },
+    []);
   return (
-    <SearchResults/>
+    <SearchResults results={results}/>
   )
 }
 
