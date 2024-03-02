0a24e629a6ffae4d59c682ab499cb2bc250d7a93
Claire Price <claire.price@mail.com>
2024-03-02 23:37:35 +0000

    add layered div positions and header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,34 +1,40 @@
 import React from 'react';
+import SearchButton from './SearchButton';
 import blackSmoke from './assets/images/02_black-smoke.png';
 import abstractLetters from './assets/images/02_abstract-letters.png';
 
 const Header = () => {
     return (
-        <div>
+        <div className='flex justify-center static'>
 
-            <div>
-                <img src={blackSmoke} className=''></img>
+            <div className='absolute top-0 z-10 bg-black w-full h-16 flex justify-center items-center'>
+                <h1 className='font-poppins font-extrabold text-5xl text-custom-red'>LYRIC JAM</h1>
             </div>
 
-            <div>
-                <img src={abstractLetters} className='shadow-md shadow-black'></img>
+            <div className='absolute top-0 z-0 w-full'>
+                <img src={blackSmoke} className='object-cover w-full h-screen' style={{ height: 'calc(100vh - 25px)'}}></img>
             </div>
 
-            <div className='bg-black'>
+            <div className='absolute top-32 z-10 w-5/6 h-96'>
+                <img src={abstractLetters} className='w-full h-full object-cover shadow-lg shadow-black'></img>
+            </div>
+
+            <div className='absolute top-48 bg-black shadow-lg shadow-black w-3/4 h-96 z-20'>
                 <div>
                     <div className='font-poppins text-4xl text-custom-pink text-left font-extrabold'>
                         <h2><span className='text-white'>SEARCH</span> FOR AN ARTIST</h2>
                         <h2><span className='text-white'>SELECT</span> YOUR SONG</h2>
                         <h2><span className='text-white'>VIEW</span> VIDEO / LYRICS / SONGS / FACTS</h2>
                     </div>
-                    <form>
-                        <input type='text' className='font-poppins text-center text-2xl font-extrabold placeholder-custom-red border-4 border-custom-red' placeholder='SEARCH ARTIST'></input>
-                        <button type='submit' className='bg-custom-red border-4 border-custom-red'><p className='text-custom-red text-2xl'>******</p></button>
-                    </form>
+                    <SearchButton />
                 </div>
             </div>
 
+            
         </div>
+            
+
+        
     );
 };
 
