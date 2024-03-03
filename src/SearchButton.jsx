adc363419a55398ced4bce9509213a3d83679325
Claire Price <claire.price@mail.com>
2024-03-03 20:42:23 +0000

    add media queries

--- src/SearchButton.jsx
+++ src/SearchButton.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 
 const SearchButton = () => {
     return (
-        <div className=''>
-            <form>
-                <input type='text' className='font-poppins font-extrabold text-center text-2xl placeholder-custom-red border-4 border-custom-red' placeholder='SEARCH ARTIST'></input>
-                <button type='submit' className='bg-custom-red '><p className='font-poppins text-custom-red text-2xl border-4 border-custom-red'>******</p></button>
-            </form>
-        </div>
+        <form>
+            <input type='text' className='font-poppins font-extrabold text-center text-base sm:text-2xl placeholder-custom-red border-4 border-custom-red w-36 sm:w-60' placeholder='SEARCH ARTIST'></input>
+            <button type='submit' className='bg-custom-red '><p className='font-poppins text-custom-red text-base sm:text-2xl border-4 border-custom-red w-12 sm:w-16'>*</p></button>
+        </form>
     );
 };
 
