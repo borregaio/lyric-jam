08a28025e03ca6c27f169c092aec24af34e88d2e
Claire Price <claire.price@mail.com>
2024-03-03 22:49:43 +0000

    Header responsive

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,28 +5,31 @@ import abstractLetters from './assets/images/02_abstract-letters.png';
 
 const Header = () => {
     return (
-        <div className='flex justify-center static'>
+        <div className='flex justify-center h-headerHeight0 sm:h-headerHeight1 relative'>
 
             <div className='absolute top-0 z-10 bg-black w-full h-16 flex justify-center items-center'>
                 <h1 className='font-poppins font-extrabold text-custom-red text-4xl sm:text-5xl'>LYRIC JAM</h1>
             </div>
 
-            <div className='absolute top-0 z-0 w-full'>
-                <img src={blackSmoke} className='object-cover w-full h-screen' style={{ height: 'calc(100vh - 25px)'}}></img>
+            <div className='absolute w-full h-headerHeight0 sm:h-headerHeight1' >
+                <img src={blackSmoke} className='w-full h-full object-cover' ></img>
             </div>
 
-            <div className='absolute top-24 sm:top-36 z-10 w-5/6 h-3/5'>
+            <div className='absolute top-24 sm:top-36 z-10 w-5/6 h-96 flex-auto'>
                 <img src={abstractLetters} className='w-full h-full object-cover shadow-lg shadow-black'></img>
             </div>
 
-            <div className='absolute top-32 sm:top-52 bg-black shadow-lg shadow-black w-3/4 h-3/5 z-20 flex justify-center items-center' >
-                <div className='sm:w-5/6 sm:h-2/3 flex flex-col bg-white'>
-                    <div className='font-poppins font-extrabold p-2 text-custom-pink text-left text-2xl md:text-4xl bg-custom-red'>
-                        <h2 className='leading-relaxed'><span className='text-white'>SEARCH</span> FOR AN ARTIST</h2>
+            <div className='absolute top-32 sm:top-52 bg-black shadow-lg shadow-black w-3/4 h-96 z-20 flex-auto' >
+                <div className='w-full h-full flex flex-col'>
+                    <div className='basis-2/12 flex-auto'></div>
+                    <div className='font-poppins font-extrabold px-4 sm:px-20 text-custom-pink text-left text-2xl md:text-4xl basis-4/12 flex-auto'>
+                        <h2 className=' leading-relaxed'><span className='text-white'>SEARCH</span> FOR AN ARTIST</h2>
                         <h2 className='leading-relaxed'><span className='text-white'>SELECT</span> YOUR SONG</h2>
                         <h2 className='leading-relaxed'><span className='text-white'>VIEW</span> VIDEO / LYRICS / SONGS / FACTS</h2>
                     </div>
-                    <div className='items-center'><SearchButton /></div>
+                    <div className='basis-2/12 flex-auto'></div>
+                    <div className='basis-1/12 flex justify-center items-center flex-auto'><SearchButton /></div>
+                    <div className='basis-2/12 flex-auto'></div>
                 </div>
             </div>
 
