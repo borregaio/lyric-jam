32ebd106c13bc479bab0fb02a366e4c6dc182e91
Claire Price <claire.price@mail.com>
2024-03-04 12:56:45 +0000

    add Facts structure

---
+++ src/Facts.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Facts = () => {
+    return (
+        <div>
+            <div className='font-poppins pt-20 pb-8 text-4xl font-extrabold text-left'>
+                FACTS
+            </div>
+
+            <div className='grid grid-cols-2'>
+
+                <div className='font-poppins py-8 pr-8 text-left'>
+                    <p>
+                    “TEXAS HOLD ‘EM” is one of Beyonce’s surprise single from a her upcoming country-themed album, Renaissance act II. It was teased during her Super Bowl LVIII commercial and shortly released after.
+                    The country song has heavy western and southern influences and can be seen as a love letter to the south and Beyoncé’s southern roots. The song features a banjo and viola played by Rhiannon Giddens.
+                    </p>
+                    about song
+                </div>
+
+                <div className='font-poppins p-8 text-left bg-custom-red shadow-lg shadow-black'>
+                    <p>
+                    Beyoncé Giselle Knowles-Carter, born September 4, 1981, better known professionally by her first name Beyoncé, is an American singer, rapper, songwriter, producer, actress and businesswoman.
+                    In the twenty-first century, no pop star was as poised, as polished, or as generally fierce as Beyoncé. She scored early success with Destinys Child, who started out as a sexier and sassier version of TLC, then steadily became more and more of a vehicle for Beyoncés operatic vocals and general diva-tude, which may have been the plan all along.
+                    </p>
+                </div>
+
+            </div>    
+
+        </div>
+    );
+}
+
+export default Facts;
