af33b9a9383fff9f95611c0ae6e54dd83a50580e
jkp1990 <jessicapourawal@gmail.com>
2024-03-04 19:25:53 +0000

    added About section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Hero from './Hero';
+import About from './About';
 import SearchResults from './SearchResults';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Hero />
       <SearchResults />
+      <About />
     </React.Fragment>
   );
 }
