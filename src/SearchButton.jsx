ac772e24ec591c13118e3e2102d327c5e8fdfa87
Claire Price <claire.price@mail.com>
2024-03-04 21:11:41 +0000

    add hover effect

--- src/SearchButton.jsx
+++ src/SearchButton.jsx
@@ -4,7 +4,7 @@ const SearchButton = () => {
     return (
         <form>
             <input type='text' className='font-poppins font-extrabold text-center text-base sm:text-2xl placeholder-custom-red border-4 border-custom-red w-36 sm:w-60' placeholder='SEARCH ARTIST'></input>
-            <button type='submit' className='bg-custom-red '><p className='font-poppins text-custom-red text-base sm:text-2xl border-4 border-custom-red w-12 sm:w-16'>*</p></button>
+            <button type='submit' className='bg-custom-red hover:bg-white'><p className='font-poppins text-custom-red text-base sm:text-2xl border-4 border-custom-red w-12 sm:w-16  hover:text-white'>*</p></button>
         </form>
     );
 };
