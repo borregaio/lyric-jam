ff249016369e38e0baff08922954a479a6a86518
sufgaz <sufgaz911@gmail.com>
2024-03-06 20:24:31 +0000

    added CSS and fixes to player

--- src/Components/YouTubeEmbed.jsx
+++ src/Components/YouTubeEmbed.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const YouTubeEmbed = ({ videoId }) => {
   return (
-    <div className="youtube-embed-container">
+    <div className="youtube-embed-container mx-auto max-w-4xl">
       <iframe
-        width="560"
-        height="315"
-        src={`https://www.youtube.com/embed/${videoId}`}
+        className="w-full"
+        style={{ height: '500px' }}
+        src={`https://www.youtube.com/embed/jCOX8dT9q8M?si=FEdGbtf9x7KKWoU-`}
         title="YouTube video player"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; webshare"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
       ></iframe>
     </div>
