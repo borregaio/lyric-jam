659f317217c9f5e46d3bed328fa365eca0ea169e
Claire Price <claire.price@mail.com>
2024-03-07 19:53:30 +0000

    removed fixed

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="bg-custom-red text-custom-gray text-center p-4 fixed bottom-0 min-w-full z-50 relative">
+        <div>
+        <footer className="bg-custom-red text-custom-gray text-center p-4 bottom-0 min-w-full relative">
             <i className="fa-brands fa-square-facebook px-2 text-3xl"></i>
             <i className="fa-brands fa-square-instagram px-2 text-3xl"></i>
             <i className="fa-brands fa-square-snapchat px-2 text-3xl"></i>
         </footer>
+        </div>
     );
 };
 
